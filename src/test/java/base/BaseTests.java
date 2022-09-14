746a9a836f3084c6fb3fcd2fa041e528ce2db3cc
GetForMe <silenko365@gmail.ocm>
2022-09-14 13:47:50 +0300

    First commit

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,37 @@
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+
+import java.util.List;
+
+public class BaseTests {
+    private WebDriver driver;
+    protected HomePage homePage;
+
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+
+      /*  WebElement inputsLinks = driver.findElement(By.linkText("Shifting Content"));
+        inputsLinks.click();
+        WebElement inputsLink1 = driver.findElement(By.linkText("Example 1: Menu Element"));
+        List<WebElement> links = driver.findElements(By.tagName("li"));
+        System.out.println("Number of links " + links.size());
+      */
+        homePage = new HomePage(driver);
+
+        //System.out.println(driver.getTitle());
+        //driver.quit();
+    }
+
+    public static void main(String[] args) {
+        BaseTests tests=new BaseTests();
+        tests.setUp();
+    }
+}
